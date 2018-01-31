ee45e4a2740c190b314990b652b1a724dd2e0e98
Nikithareddy168 <nikitha.reddy168@gmail.com>
2018-01-30 23:50:14 -0500

    Add code to print "Baz"

--- src/FooBarBaz.java
+++ src/FooBarBaz.java
@@ -4,8 +4,32 @@ public class FooBarBaz {
 	FooBarBaz(String entry){
 		int countUpTo = Integer.parseInt(entry);
 		System.out.println("upper Limit ========"+countUpTo);
-		for(int i = 1; i <= 110 ;i++){
-			// Upper limit set as 110
+		for(int i = 1; i <= countUpTo ;i++){
+			if(i % 3 == 0 && i % 5 == 0 && i % 7 == 0){
+				System.out.println("FooBarBaz");
+			}
+			else if(i % 3 == 0 && i % 5 == 0){
+				System.out.println("FooBar");
+			}
+			else if(i % 5 == 0 && i % 7 == 0){
+				System.out.println("BarBaz");
+			}
+			else if(i % 3==0 && i % 7 == 0){
+				System.out.println("FooBaz");
+			}
+			else if(i % 3 == 0){
+				System.out.println("Foo");
+			}
+			else if(i % 5 == 0){
+				System.out.println("Bar");
+			}
+			else if(i % 7 == 0){
+				System.out.println("Baz");
+			}
+			else{
+				System.out.println(i);
+			}
+			
 		}
 	}
 	
