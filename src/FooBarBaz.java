bbebe0c387caac90e180b9c632897cae74389131
Nikithareddy168 <nikitha.reddy168@gmail.com>
2018-02-13 20:39:42 -0500

    Refactor code to make it testable

--- src/FooBarBaz.java
+++ src/FooBarBaz.java
@@ -5,10 +5,35 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class FooBarBaz {
-    FooBarBaz(int countUpTo) throws IOException {
-        // int countUpTo = Integer.parseInt(entry);
-        // System.out.println("upper Limit ========"+countUpTo);
-        // List<String> ls = new ArrayList<String>();
+   public void run() throws IOException{
+       while(true){
+           Scanner scanner = new Scanner(System.in);
+           System.out.println("enter a number : ");
+           String entry = scanner.next();
+           validateinput(entry);
+       }
+     
+    
+   }
+
+    private void validateinput(String entry) throws IOException {
+        if ("stop".equalsIgnoreCase(entry)) {
+            System.exit(0);
+        }
+        else {
+            try {
+                int num = Integer.parseInt(entry);
+                replace(num);
+                if (num >= 110){
+                    System.out.println("Invalid Input");
+                };
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid Input");
+            }
+        }
+    }
+    private void replace(int num) throws IOException{
         File f1 = new File("d:\\foobarbaz.txt");
         if (f1.exists()) {
             f1.delete();
@@ -19,81 +44,69 @@ public class FooBarBaz {
         }
         FileWriter fw = new FileWriter(f, true); // true is for append
         BufferedWriter bw = new BufferedWriter(fw);
-        for (int i = 1; i <= countUpTo; i++) {
-            // code for "FooBarBaz"
-            try {
-                if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
-                    // System.out.println("FooBarBaz");
-                    bw.append("FooBarBaz");
-                    bw.newLine();
-                }
-                else if (i % 3 == 0 && i % 5 == 0) {
-                    // System.out.println("FooBar");
-                    bw.append("FooBar");
-                    bw.newLine();
-
-                }
-                else if (i % 5 == 0 && i % 7 == 0) {
-                    // System.out.println("BarBaz");
-                    bw.append("BarBaz");
-                    bw.newLine();
-                }
-                else if (i % 3 == 0 && i % 7 == 0) {
-                    // System.out.println("FooBaz");
-                    bw.append("FooBaz");
-                    bw.newLine();
-                }
-                else if (i % 3 == 0) {
-                    // System.out.println("Foo");
-                    bw.append("Foo");
-                    bw.newLine();
-                }
-                else if (i % 5 == 0) {
-                    // System.out.println("Bar");
-                    bw.append("Bar");
-                    bw.newLine();
-                }
-                else if (i % 7 == 0) {
-                    // System.out.println("Baz");
-                    bw.append("Baz");
-                    bw.newLine();
-                }
-                else {
-                    // System.out.println(i);
-                    bw.append(Integer.toString(i));
-                    bw.newLine();
-                }
-            }
-            catch (Exception ex) {
-
-                ex.printStackTrace();
-
-            }
-        }
-        bw.close();
-    }
-
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        while (true) { // running the loop until the user enters stop
-            System.out.println("enter a number : ");
-            String entry = scanner.next();
-            if ("stop".equalsIgnoreCase(entry)) {
-                System.exit(0);
-            }
-            else {
+        for (int i = 1; i <= num; i++) {
+      
+                // code for "FooBarBaz"
                 try {
-                    int num = Integer.parseInt(entry);
-                    new FooBarBaz(num);
-                    if (num >= 110){
-                        System.out.println("Invalid Input");
-                    };
+                    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+                        // System.out.println("FooBarBaz");
+                        bw.append("FooBarBaz");
+                        bw.newLine();
+                    }
+                    else if (i % 3 == 0 && i % 5 == 0) {
+                        // System.out.println("FooBar");
+                        bw.append("FooBar");
+                        bw.newLine();
+
+                    }
+                    else if (i % 5 == 0 && i % 7 == 0) {
+                        // System.out.println("BarBaz");
+                        bw.append("BarBaz");
+                        bw.newLine();
+                    }
+                    else if (i % 3 == 0 && i % 7 == 0) {
+                        // System.out.println("FooBaz");
+                        bw.append("FooBaz");
+                        bw.newLine();
+                    }
+                    else if (i % 3 == 0) {
+                        // System.out.println("Foo");
+                        bw.append("Foo");
+                        bw.newLine();
+                    }
+                    else if (i % 5 == 0) {
+                        // System.out.println("Bar");
+                        bw.append("Bar");
+                        bw.newLine();
+                    }
+                    else if (i % 7 == 0) {
+                        // System.out.println("Baz");
+                        bw.append("Baz");
+                        bw.newLine();
+                    }
+                    else {
+                        // System.out.println(i);
+                        bw.append(Integer.toString(i));
+                        bw.newLine();
+                    }
                 }
-                catch (NumberFormatException e) {
-                    System.out.println("Invalid Input");
+                catch (Exception ex) {
+
+                    ex.printStackTrace();
+
                 }
             }
+            bw.close();
         }
+            
+        
+    
+
+    public static void main(String[] args) throws IOException{
+       
+        FooBarBaz fbb = new FooBarBaz();
+        fbb.run();
+        
     }
 
 }
